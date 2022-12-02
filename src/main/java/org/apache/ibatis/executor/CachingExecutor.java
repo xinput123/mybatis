7d15f3ca00f1fe33a5818cd3ed89fead89cebe58
im.xinput <yuan.lai@immomo.com>
2022-12-02 22:32:22 +0800

    first commit

---
+++ src/main/java/org/apache/ibatis/executor/CachingExecutor.java
@@ -0,0 +1,181 @@
+/**
+ *    Copyright 2009-2021 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.executor;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.cache.Cache;
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.cache.TransactionalCacheManager;
+import org.apache.ibatis.cursor.Cursor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.mapping.ParameterMode;
+import org.apache.ibatis.mapping.StatementType;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.transaction.Transaction;
+
+/**
+ * @author Clinton Begin
+ * @author Eduardo Macarron
+ */
+public class CachingExecutor implements Executor {
+
+    private final Executor delegate;
+    private final TransactionalCacheManager tcm = new TransactionalCacheManager();
+
+    public CachingExecutor(Executor delegate) {
+        this.delegate = delegate;
+        delegate.setExecutorWrapper(this);
+    }
+
+    @Override
+    public Transaction getTransaction() {
+        return delegate.getTransaction();
+    }
+
+    @Override
+    public void close(boolean forceRollback) {
+        try {
+            // issues #499, #524 and #573
+            if (forceRollback) {
+                tcm.rollback();
+            } else {
+                tcm.commit();
+            }
+        } finally {
+            delegate.close(forceRollback);
+        }
+    }
+
+    @Override
+    public boolean isClosed() {
+        return delegate.isClosed();
+    }
+
+    @Override
+    public int update(MappedStatement ms, Object parameterObject) throws SQLException {
+        flushCacheIfRequired(ms);
+        return delegate.update(ms, parameterObject);
+    }
+
+    @Override
+    public <E> Cursor<E> queryCursor(MappedStatement ms, Object parameter, RowBounds rowBounds) throws SQLException {
+        flushCacheIfRequired(ms);
+        return delegate.queryCursor(ms, parameter, rowBounds);
+    }
+
+    @Override
+    public <E> List<E> query(MappedStatement ms, Object parameterObject, RowBounds rowBounds, ResultHandler resultHandler) throws SQLException {
+        // 获取BoundSql对象并创建相应的CacheKey
+        BoundSql boundSql = ms.getBoundSql(parameterObject);
+        CacheKey key = createCacheKey(ms, parameterObject, rowBounds, boundSql);
+        return query(ms, parameterObject, rowBounds, resultHandler, key, boundSql);
+    }
+
+    @Override
+    public <E> List<E> query(MappedStatement ms, Object parameterObject, RowBounds rowBounds, ResultHandler resultHandler, CacheKey key, BoundSql boundSql)
+            throws SQLException {
+        Cache cache = ms.getCache(); // 获取该命名空间使用的二级缓存
+        if (cache != null) { // 是否开启了二级缓存功能
+            flushCacheIfRequired(ms); // 根据<select>标签配置决定是否需要清空二级缓存
+            // 检测useCache配置以及是否使用了resultHandler配置
+            if (ms.isUseCache() && resultHandler == null) {
+                ensureNoOutParams(ms, boundSql); // 是否包含输出参数
+                // 查询二级缓存
+                List<E> list = (List<E>) tcm.getObject(cache, key);
+                if (list == null) {
+                    // 二级缓存未命中，通过被装饰的Executor对象查询结果对象
+                    list = delegate.query(ms, parameterObject, rowBounds, resultHandler, key, boundSql);
+                    // 将查询结果保存到TransactionalCache.entriesToAddOnCommit集合中暂存
+                    tcm.putObject(cache, key, list);
+                }
+                return list;
+            }
+        }
+        // 如果未开启二级缓存，直接通过被装饰的Executor对象查询结果对象
+        return delegate.query(ms, parameterObject, rowBounds, resultHandler, key, boundSql);
+    }
+
+    @Override
+    public List<BatchResult> flushStatements() throws SQLException {
+        return delegate.flushStatements();
+    }
+
+    @Override
+    public void commit(boolean required) throws SQLException {
+        delegate.commit(required);
+        tcm.commit();
+    }
+
+    @Override
+    public void rollback(boolean required) throws SQLException {
+        try {
+            delegate.rollback(required);
+        } finally {
+            if (required) {
+                tcm.rollback();
+            }
+        }
+    }
+
+    private void ensureNoOutParams(MappedStatement ms, BoundSql boundSql) {
+        if (ms.getStatementType() == StatementType.CALLABLE) {
+            for (ParameterMapping parameterMapping : boundSql.getParameterMappings()) {
+                if (parameterMapping.getMode() != ParameterMode.IN) {
+                    throw new ExecutorException("Caching stored procedures with OUT params is not supported.  Please configure useCache=false in " + ms.getId() + " statement.");
+                }
+            }
+        }
+    }
+
+    @Override
+    public CacheKey createCacheKey(MappedStatement ms, Object parameterObject, RowBounds rowBounds, BoundSql boundSql) {
+        return delegate.createCacheKey(ms, parameterObject, rowBounds, boundSql);
+    }
+
+    @Override
+    public boolean isCached(MappedStatement ms, CacheKey key) {
+        return delegate.isCached(ms, key);
+    }
+
+    @Override
+    public void deferLoad(MappedStatement ms, MetaObject resultObject, String property, CacheKey key, Class<?> targetType) {
+        delegate.deferLoad(ms, resultObject, property, key, targetType);
+    }
+
+    @Override
+    public void clearLocalCache() {
+        delegate.clearLocalCache();
+    }
+
+    private void flushCacheIfRequired(MappedStatement ms) {
+        Cache cache = ms.getCache();
+        if (cache != null && ms.isFlushCacheRequired()) {
+            tcm.clear(cache);
+        }
+    }
+
+    @Override
+    public void setExecutorWrapper(Executor executor) {
+        throw new UnsupportedOperationException("This method should not be called");
+    }
+
+}
---
+++ src/main/java/org/apache/ibatis/plugin/Interceptor.java
@@ -0,0 +1,37 @@
+/**
+ *    Copyright 2009-2021 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.plugin;
+
+import java.util.Properties;
+
+/**
+ * @author Clinton Begin
+ */
+public interface Interceptor {
+
+  // 插件实现类中需要实现的拦截逻辑
+  Object intercept(Invocation invocation) throws Throwable;
+
+  // 在该方法中会决定是否触发intercept()方法
+  default Object plugin(Object target) {
+    return Plugin.wrap(target, this);
+  }
+
+  default void setProperties(Properties properties) {
+    // 在整个MyBatis初始化过程中用来初始化该插件的方法
+  }
+
+}
